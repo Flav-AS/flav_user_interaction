fe90fbb21725d0d0f64c9d9103fdd7beede3bf9c
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2025-11-18 20:39:35 +0200

    Add middleware and restriction pages

--- auth.ts
+++ auth.ts
@@ -1,5 +1,12 @@
 import NextAuth from "next-auth"
+import MicrosoftEntraID from "next-auth/providers/microsoft-entra-id"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    MicrosoftEntraID({
+      clientId: process.env.MICROSOFT_ENTRA_ID_CLIENT_ID!,
+      clientSecret: process.env.MICROSOFT_ENTRA_ID_CLIENT_SECRET!,
+      issuer: `https://login.microsoftonline.com/${process.env.MICROSOFT_ENTRA_ID_TENANT_ID}/v2.0`,
+    })
+  ],
 })
--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,31 @@
-export { auth as middleware } from "@/auth"
+import { auth } from "@/auth"
+import { NextResponse } from "next/server"
+
+export { auth } from "@/auth"
+
+const protectedRoutes = ["/dashboard", "/profile"]
+const apiAuthPrefix = "/api/auth/"
+const authPageRoutes = ["/login"]
+
+export default auth((req) => {
+  const { nextUrl } = req
+  const isLoggedIn = !!req.auth
+
+  const path = nextUrl.pathname
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
+  const isProtectedRoute = protectedRoutes.includes(path)
+  const isAuthPageRoute = authPageRoutes.includes(path)
+
+  if (isApiAuthRoute) {
+    return NextResponse.next()
+  }
+  if (isProtectedRoute && !isLoggedIn) {
+    return NextResponse.redirect(new URL("/login", req.nextUrl))
+  }
+
+  return NextResponse.next();
+})
+
+export const config = {
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+}
