31ae754c0265d46b6a3778cfb023eeec130baabc
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2025-11-18 21:01:51 +0200

    Add basic UI and data integration

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+"use client";
 
-function Dashboard() {
+import { useSession } from "next-auth/react";
+
+export default function Dashboard() {
+  const session = useSession();
   return (
-    <div>Dashboard</div>
-  )
+    <div>
+      <p>dashboard page </p>
+      <p>User email: {session.data?.user?.email}</p>
+    </div>
+  );
 }
-
-export default Dashboard
