922794330502257a8ad204643ba3e36baa710c68
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2025-12-11 04:13:19 +0200

    Show group actions

--- components/accounts/AccountsTreeView.tsx
+++ components/accounts/AccountsTreeView.tsx
@@ -132,25 +132,23 @@ function TreeNode({
         </div>
 
         {/* Actions */}
-        <div className="flex items-center gap-1 opacity-0 group-hover:opacity-100 transition-opacity">
+        <div className="flex items-center gap-1 group-hover:opacity-100 transition-opacity">
           <button
             onClick={(e) => {
               e.stopPropagation();
               setIsEditing(true);
             }}
-            className="p-1 text-gray-700 bg-green-400 hover:text-blue-600 text-xs"
+            className="p-1 text-gray-600 hover:text-blue-600 text-xs"
             title="Rename"
           >
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-fill text-gray-700 hover:text-blue-600" viewBox="0 0 16 16">
-              <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
-            </svg>
+            ✎
           </button>
           <button
             onClick={(e) => {
               e.stopPropagation();
               setIsEditingParent(!isEditingParent);
             }}
-            className="p-1 text-gray-700 hover:text-green-600 text-xs"
+            className="p-1 text-gray-600 hover:text-green-600 text-xs"
             title="Change parent"
           >
             ↑
@@ -162,10 +160,10 @@ function TreeNode({
                 onDeleteGroup(group.id);
               }
             }}
-            className="p-1 text-gray-700 hover:text-red-600 text-xs"
+            className="p-1 text-gray-600 hover:text-red-600 text-xs"
             title="Delete"
           >
-            X
+            ✕
           </button>
         </div>
       </div>
